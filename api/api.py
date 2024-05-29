12c356bbe89a59dbbb99039a9b46f831c075dc7c
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2024-05-29 20:52:05 +0300

    Add class

---
+++ api/api.py
@@ -0,0 +1,14 @@
+import requests
+
+class API:
+    def __init__(self, domen: str):
+        self.domen = domen
+
+    def send_request(self, url: str, method: str, headers: dict = None, params: dict = None) -> None:
+
+
+
+    def get_grenade(self) -> dict:
+        pass
+
+
